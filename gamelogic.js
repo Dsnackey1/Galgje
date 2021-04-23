7d877591078ccb13a5d692a2ed182a7d9e1ce4e4
des-92git config --global user.name des-92git config --global user.name des-92 <nsnackey@outlook.com>
2021-04-23 10:49:26 +0200

    Galgje

--- gamelogic.js
+++ gamelogic.js
@@ -1,13 +1,62 @@
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
+  
+  let displayedWord = "";
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    const isGuessed = guesses.includes(letter); // guesses.includes(word[i]);
+
+    if (isGuessed) {
+      displayedWord += letter + " ";
+    } else {
+      displayedWord += "_ ";
+    }
+  }
+  return displayedWord;
 }
+// console.log(displayWordSoFar("javascript", []));
+
 
 function isGameWon(word, guesses) {
-  // WRITE ME
+  
+  let displayedWord = "";
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    const isGuessed = guesses.includes(letter); // guesses.includes(word[i]);
+
+    if (isGuessed) {
+      displayedWord += letter + " ";
+    } else {
+      displayedWord += "_ ";
+    }
+  } 
+  
+  if (!displayedWord.includes("_")) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
+
 function isGameLost(word, guesses) {
-  // WRITE ME
+  
+  let counter = 0;
+  
+  for (let i = 0; i < guesses.length; i++) {
+    const guessedLetter = guesses[i];
+    const isGuessed = word.includes(guessedLetter);
+
+    if (!isGuessed) {
+      counter += 1;
+    } 
+  }
+  if (counter === 7) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const { question } = require("readline-sync");
 const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
 
 function game(word, guesses) {
+  
+  if (isGameWon(word, guesses)) {
+    console.log("je hebt Gewonnen");
+    return;
+  }
+
+  if (isGameLost(word, guesses)) {
+    console.log("Je hebt verloren");
+    return;
+  }
+
+
   console.log("Dit heb je tot nu toe geraden: ", guesses);
+  
+  console.log(displayWordSoFar(word, guesses));
 
   const letter = question("Raad een letter: ");
 
@@ -12,6 +26,7 @@ function game(word, guesses) {
   // volgende ronde! we roepen game nog een keer aan
   game(word, guesses);
 }
+
 
 console.log(`
 __________  
